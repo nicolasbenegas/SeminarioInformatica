dd416ae531e32ae0fd83793e1a665820c6d018ab
nicolasbenegas <nicolasbenegasmovil@gmail.com>
2026-06-27 21:50:22 -0300

    Eliminacion de scripts SQL desactualizados

--- Borrado_Tablas_SQL.sql
+++
@@ -1,8 +0,0 @@
---Eliminar un turno
-DELETE FROM turno
-WHERE id = 1;
-
---Suspender un usuario (borrado lógico)
-UPDATE usuario_sistema
-SET activo = FALSE
-WHERE id = 10;
--- Consulta_Tablas_SQL.sql
+++
@@ -1,9 +0,0 @@
---Consultar todos los pacientes
-SELECT u.id, u.dni, u.nombre, u.apellido
-FROM usuario u
-INNER JOIN paciente p ON u.id = p.id;
-
---Consultar turnos de un paciente
-SELECT *
-FROM turno
-WHERE paciente_id = 1;
